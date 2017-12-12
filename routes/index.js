921378a6e94ed6d2644c9f8897580a9434db9856
Tanner <tanner.jensen08@gmail.com>
2017-12-12 18:01:15 +0000

    added session and login code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var expressSession = require('express-session');
 
-/* GET home page. */
-router.get('/login', function(req, res, next) {
-  res.sendFile('login.html', {root: "public"});
+var users = require('../controllers/users_controller');
+console.log("before / Route");
+router.get('/', function(req, res){
+    console.log("/ Route");
+//    console.log(req);
+    console.log(req.session);
+    if (req.session.user) {
+      console.log("/ Route if user");
+      res.render('index', {username: req.session.username,
+                           msg:req.session.msg,
+                           color:req.session.color});
+    } else {
+      console.log("/ Route else user");
+      req.session.msg = 'Access denied!';
+      res.redirect('/login');
+    }
 });
+router.get('/multi', function(req, res){
+    console.log("/multi Route");
+    if (req.session.user) {
+      res.render('multi', {username: req.session.username});
+    } else {
+      req.session.msg = 'Access denied!';
+      res.redirect('/login');
+    }
+});
+router.get('/signup', function(req, res){
+    console.log("/signup Route");
+    if(req.session.user){
+      res.redirect('/');
+    }
+    res.render('signup', {msg:req.session.msg});
+});
+router.get('/login',  function(req, res){
+    console.log("/login Route");
+    if(req.session.user){
+      res.redirect('/');
+    }
+    res.render('login', {msg:req.session.msg});
+});
+router.get('/logout', function(req, res){
+    console.log("/logout Route");
+    req.session.destroy(function(){
+      res.redirect('/login');
+    });
+  });
+router.post('/signup', users.signup);
+router.post('/user/update', users.updateUser);
+router.post('/user/delete', users.deleteUser);
+router.post('/login', users.login);
+router.get('/user/profile', users.getUserProfile);
+
 
 module.exports = router;
